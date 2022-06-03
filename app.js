d0b09fa12e6744b12cbef8544027675ad2d58968
Irene Waweru <85219856+irenenjoki@users.noreply.github.com>
2022-06-03 09:47:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import {translate} from './app/translator.js';
-import {Math} from './app/math.js';
 const port=7000;
 const app = express();
 app.get('/', function(req,res){
@@ -12,12 +11,7 @@ app.get('/', function(req,res){
 app.get('/translate', function(req,res){
 const word=req.query.word
     res.send(translate(word))});
-    app.get('/math', function(req,res){
-        const num = Math.floor(Math.random() * (max - min + 1)) + min;
-        return num;
-        random(1, 10);
-
-    });
+    
 app.listen(port,function(){
 console.log('Server started on port 7000...');
 });
